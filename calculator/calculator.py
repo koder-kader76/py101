dade240a11f4eebc24e95afb7be803f754059070
Abdul Kader <koder.kader76@gmail.com>
2025-04-10 18:08:49 +0800

    Lesson 2: Calculator Walk-Through

---
+++ calculator/calculator.py
@@ -0,0 +1,36 @@
+# Walk-through: Calculator 
+
+# Our first program in this course will be a command line calculator program that will:
+
+# Ask the user for the first number
+# Ask the user for the second number
+# Ask the user for an operation to perform
+# Perform the operation on the two numbers
+# Print the result
+
+print("Welcome to Calculator!")
+
+print("What's the first number? ")
+number1 = input()
+
+print("What's the second number? ")
+number2 = input()
+
+print(f"{number1} {number2}")
+
+print(
+    "What operation would you like to perform?\n"
+    "1) Add 2) Subtract 3) Multiply 4) Divide"
+)
+operation = input()
+
+if operation == '1': # 1 represents addition
+    output = int(number1) + int(number2)
+elif operation == '2': # 2 represents subtraction
+    output = int(number1) - int(number2)
+elif operation == '3': # 3 represents multiplication
+    output = int(number1) * int(number2)
+elif operation == '4': # 4 represents divisio
+    output = int(number1) / int(number2)
+
+print(f"The result is: {output}")
