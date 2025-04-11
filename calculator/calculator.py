146df8628a8efe74d2215b3189d6c5bd9f254159
Abdul Kader <koder.kader76@gmail.com>
2025-04-11 19:22:42 +0800

    Lesson 2: Refactoring Calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -8,29 +8,54 @@
 # Perform the operation on the two numbers
 # Print the result
 
-print("Welcome to Calculator!")
+def prompt(message):
+    print(f"==> {message}")
 
-print("What's the first number? ")
+def invalid_number(number):
+    try:
+        int(number)
+    except ValueError:
+        return True
+
+    return False
+
+prompt("Welcome to Calculator!")
+
+prompt("What's the first number? ")
 number1 = input()
 
-print("What's the second number? ")
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number? ")
 number2 = input()
 
-print(f"{number1} {number2}")
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
 
-print(
+prompt(f"{number1} {number2}")
+
+prompt(
     "What operation would you like to perform?\n"
     "1) Add 2) Subtract 3) Multiply 4) Divide"
 )
 operation = input()
 
-if operation == '1': # 1 represents addition
-    output = int(number1) + int(number2)
-elif operation == '2': # 2 represents subtraction
-    output = int(number1) - int(number2)
-elif operation == '3': # 3 represents multiplication
-    output = int(number1) * int(number2)
-elif operation == '4': # 4 represents divisio
-    output = int(number1) / int(number2)
+while operation not in ['1', '2', '3', '4']:
+    prompt("You must choose 1, 2, 3, 4")
+    operation = input()
 
-print(f"The result is: {output}")
+
+match operation:
+    case '1': # 1 represents addition
+        output = int(number1) + int(number2)
+    case '2': # 2 represents subtraction
+        output = int(number1) - int(number2)
+    case '3': # 3 represents multiplication
+        output = int(number1) * int(number2)
+    case '4': # 4 represents divisio
+        output = int(number1) / int(number2)
+
+prompt(f"The result is: {output}")
